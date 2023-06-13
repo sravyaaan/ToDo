c38f7890d7efdd00b2834fdac77c2187f7236012
helloish <ishunamburu@gmail.com>
2023-06-13 08:25:07 -0700

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Sravya Namburu on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
